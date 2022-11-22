c8d1f1379cf387de44979c3ded9d7457a4e0d85a
Ilham <117557607+Ilxamer@users.noreply.github.com>
2022-11-22 14:02:33 +0300

    Delete Module_3_part_2.py

--- Module_3_part_2.py
+++
@@ -1,9 +0,0 @@
-# #Task_1
-list_1 = ['1','2','5','python 2.0','python 3.0','hi','python 3.0','5','hi']
-list_2 = []
-for i in list_1:
-	if i not in list_2:
-		list_2.append(i)
-
-print(list_1,list_2)
-
